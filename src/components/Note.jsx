918dddf763dbcf354f9e006e21719a7849df6a3f
tomzie27s <tomzie27s@gmail.com>
2020-11-10 21:34:49 -0500

    Initial Upload

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+    return (
+        <div className="note">
+            <h1>Title Here</h1>
+            <p>Content Here</p>
+        </div>
+    );
+};
+
+export default Note;
